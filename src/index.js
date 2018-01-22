8a0502602440201467a067058e7abbafb6cd4b78
Patricia Garcia <pat@patriciagarcia.me>
2018-01-22 19:43:41 +0100

    fix: make translator function available via the module
    Moves translator to its own module and exposes it to angular apps
    depending on angular-nav-data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import angular from 'angular'
 
 import { default as locationsModuleName } from './locations/locations.module'
 import { default as productsModuleName } from './products/products.module'
+import { default as translatorModuleName } from './translator/translator.module'
 
 angular.module('angularNavData', [
   locationsModuleName,
-  productsModuleName
+  productsModuleName,
+  translatorModuleName
 ])
